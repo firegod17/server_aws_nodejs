7112234d2aee436995e94980b4b805bbc0cf1d17
Sergei Kachivski <sergeikachivski@MacBook-Pro-Sergei.local>
2020-09-29 13:38:19 +0300

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello World !')
+    res.send('Hello World !!')
 })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
